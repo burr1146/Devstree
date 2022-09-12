e3792a5aeb7229051ade77880cc96735da4e293b
burr1146 <107100806+burr1146@users.noreply.github.com>
2022-09-12 10:41:12 +0530

    Add files via upload

---
+++ devstreeWebPage/javascript/test.js
@@ -0,0 +1,238 @@
+
+
+// printing name, surname and age
+// let Name = "Burhan";
+// let Surname = "Rampura";
+// let Age = 21;
+
+// console.log("my Name is ",Name );
+// console.log("my Surname is ",Surname );
+// console.log("my Age is ",Age );
+
+
+// let
+
+// let x=5;
+// console.log(x); 
+//         //changing value
+// x=3;
+// console.log(x);
+
+//cons
+
+// const x=5
+// console.log(x);//const value cant be changed, it will throw error
+
+// let date = Date.now();
+// console.log("Today's date is ",date);
+
+
+// operators
+        // let x = 5;
+        // let y = 3;
+
+        // // addition
+        // console.log('x + y = ', x + y);  
+
+        // // subtraction
+        // console.log('x - y = ', x - y); 
+
+        // // multiplication
+        // console.log('x * y = ', x * y);  
+
+        // // division
+        // console.log('x / y = ', x / y);  
+
+        // // remainder
+        // console.log('x % y = ', x % y);   
+
+        // // increment
+        // console.log('++x = ', ++x); // x is now 6
+        // console.log('x++ = ', x++); // prints 6 and then increased to 7
+        // console.log('x = ', x);     // 7
+
+        // // decrement
+        // console.log('--x = ', --x); // x is now 6
+        // console.log('x-- = ', x--); // prints 6 and then decreased to 5
+        // console.log('x = ', x);     // 5
+
+        // //exponentiation
+        // console.log('x ** y =', x ** y);
+
+// comparision
+
+    // const a =2, b = 3;
+    // console.log(a>b);
+
+// LOgical operator 3 types && , || , !
+
+    // const a=2,b=2;
+    // console.log(a==b && a>0);
+
+// when number is added to string then number it self become string automaticaly
+    // console.log('3'+2);
+
+
+
+// when converting number to string we must use parseInt etc 
+
+    // let result;
+    // result=parseInt('23523');
+    // console.log(result);
+
+// if
+
+    // let a=2;
+    // if(a%2==0){
+    //     console.log("Even number");
+    // }
+
+// if else
+    // let a=13;
+    // if(a%2==0){
+    //     console.log("Number is Even.")
+    // }
+    // else{
+    //     console.log("Number is OddC")
+    // }
+
+// if else ladder
+
+    // let num = 3;
+
+    // if(num>0){
+    //     console.log("You have entered positive number");
+    // }
+    // else if(num<0){
+    //     console.log("You have entered odd number");
+
+    // }
+    // else{
+    //     console.log("You have entered 0");
+    // }
+
+// for loop
+    // let n=5;
+
+    // for(let i=1;i<=n;i++){
+        
+    //     console.log(i);
+
+    // }
+// switch case 
+
+    // let day = 1;
+    // switch(day){
+    //      case 1:
+    //         day = "monday";
+    //         break;
+    //      case 2:
+    //         day = "tuesday";
+    //         break;
+    //      case 3:
+    //         day = "thursday";
+    //         break;
+    //      case 4:
+    //         day = "wednesday";
+    //         break;
+    //      case 5:
+    //         day = "friday";
+    //         break;
+    //     case 5:
+    //         day = "friday";
+    //         break;
+    //         day = "friday";
+    //         break;
+    //     default:
+    //         day="holiday"
+    //         break;
+
+    // }
+    // console.log("Today's Day is ",day);
+
+
+
+    // let Day = "Friday";
+    // if(Day == "Monday"){
+    //     console.log("Today is English exam");
+    // }
+    // else if(Day=="Tuesday"){
+    //     console.log("Today is Hindi exam");
+    // }
+    // else if(Day=="Wednesday"){
+    //     console.log("Today is Maths exam");
+    // }
+    // else if(Day=="Thursday"){
+    //     console.log("Today is Science exam");
+    // }
+    // else if(Day=="Friday"){
+    //     console.log("Today is Preparation holiday.");
+    // }
+    // else if(Day=="Saturday"){
+    //     console.log("Today is Biology exam");
+    // }
+    // else{
+    //     console.log("Today is Holiday exam");
+    // }
+    
+        
+
+    let a =1234567;
+    
+    function check (num){
+        return 12.34;
+    }
+    console.log(check(a));
+
+// function
+    // function fact(num){
+    //     if(num==0){
+    //         return 1;
+    //     }
+    //     else {
+    //         return num*fact(num-1);
+    //     }
+    // }
+    //  let num=3;
+    //  console.log(fact(num));
+
+// objects 
+
+    // const me = {
+    //     name:"Burhan",
+    //     surname:"rampura",
+    //     age:21,
+    // };
+
+    // console.log(` my name is ${me.name} ${me.surname} and age is `,me.age);
+
+   
+
+// this keyword
+
+    // const animal = {
+    //     breed:"cat",
+    //     place:"rural area",
+    //     life:20,
+    //     info:function(){
+    //         console.log(`${this.breed} can live for ${this.life} years.`);
+    //      }
+
+    // };
+    // animal.info();
+// constructor 
+
+    // function car (car_name, car_model, car_price ){
+    //     this.name=car_name,
+    //     this.model=car_model,
+    //     this.price=car_price,
+    // }
+    
+
+    // const car1 = new car('Audi',2466,100000000);
+
+    // console.log(car1.name);
+
+
+
+
---
+++ devstreeWebPage/script.js
@@ -0,0 +1,23 @@
+
+
+
+$(".slider").owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000, //2000ms = 2s;
+    autoplayHoverPause: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        767:{
+            items:2
+        },
+        992:{
+            items:3
+        }
+    }
+    
+
+  });
+ 
